a6f85586e5e4f712f4ad82490da62b60c3abe990
cusengan <lordvinh1@live.com>
2017-10-21 11:03:01 -0500

    Added recommend button

--- app/src/main/java/com/prepper/prepper/MainActivity.java
+++ app/src/main/java/com/prepper/prepper/MainActivity.java
@@ -2,12 +2,19 @@ package com.prepper.prepper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mRecommend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecommend = (TextView)findViewById(R.id.recommend_text);
+
+        mRecommend.setText(R.string.recommend);
     }
 }
