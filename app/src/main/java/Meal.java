c58b24954742715b7fdad394cb136eeaa16f57fa
quandoan97 <32970828+quandoan97@users.noreply.github.com>
2017-10-21 14:47:10 -0500

    Created the Meal Class

---
+++ app/src/main/java/Meal.java
@@ -0,0 +1,89 @@
+import com.prepper.prepper.Food;
+
+import java.util.List;
+
+/**
+ * Created by Quan on 10/21/2017.
+ */
+
+public class Meal {
+
+    private String mName;
+    private String mDescription;
+    private int mCal;
+    private int mProtein;
+    private int mCarbs;
+    private int mFats;
+    private List<Food> mFoods;
+
+    public Meal(List<Food> foodList){
+        calculateCals(foodList);
+        calculateCarbs(foodList);
+        calculateProtein(foodList);
+        calculateFats(foodList);
+    }
+
+    private void calculateCals(List<Food> foodList){
+        this.mCal = 0;
+        for(Food food: foodList) {
+            this.mCal += food.getCalories();
+        }
+    }
+
+    private void calculateCarbs(List<Food> foodList){
+        this.mCarbs = 0;
+        for(Food food: foodList){
+            this.mCarbs += food.getCarbs();
+        }
+    }
+
+    private void calculateFats(List<Food> foodList){
+        this.mFats = 0;
+        for(Food food: foodList){
+            this.mFats += food.getFats();
+        }
+    }
+
+    private void calculateProtein(List<Food> foodList){
+        this.mProtein = 0;
+        for(Food food: foodList){
+            this.mProtein += food.getProtein();
+        }
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public int getmCal() {
+        return mCal;
+    }
+
+    public int getmProtein() {
+        return mProtein;
+    }
+
+    public int getmCarbs() {
+        return mCarbs;
+    }
+
+    public int getmFats() {
+        return mFats;
+    }
+
+    public List<Food> getmFoods() {
+        return mFoods;
+    }
+}
--- app/src/main/java/com/prepper/prepper/Food.java
+++ app/src/main/java/com/prepper/prepper/Food.java
@@ -8,10 +8,16 @@ public class Food {
 
     private String mFoodName;
     private double mCalories;
+    private int mFats;
+    private int mCarbs;
+    private int mProtein;
 
-    public Food(String mFoodName, double mCalories){
+    public Food(String mFoodName, double mCalories, int mFats, int mCarbs, int mProtein){
         this.mFoodName = mFoodName;
         this.mCalories = mCalories;
+        this.mCarbs = mCarbs;
+        this.mFats = mFats;
+        this.mProtein = mProtein;
     }
 
     public String getFoodName() {
@@ -28,5 +34,29 @@ public class Food {
 
     public void setCalories(double mCalories) {
         this.mCalories = mCalories;
+    }
+
+    public int getFats() {
+        return mFats;
+    }
+
+    public void setFats(int mFats) {
+        this.mFats = mFats;
+    }
+
+    public int getCarbs() {
+        return mCarbs;
+    }
+
+    public void setCarbs(int mCarbs) {
+        this.mCarbs = mCarbs;
+    }
+
+    public int getProtein() {
+        return mProtein;
+    }
+
+    public void setProtein(int mProtein) {
+        this.mProtein = mProtein;
     }
 }
