ecd2fcbc1e0b52b5d2d0a0424c2427c2fc725796
singhgagan2697 <singh.gagan2697@gmail.com>
2017-10-21 12:33:57 -0500

    Added Customer to Order

--- app/src/main/java/com/prepper/prepper/Order.java
+++ app/src/main/java/com/prepper/prepper/Order.java
@@ -10,10 +10,14 @@ public class Order {
 
     private int mOrderNumber;
     private List<Food> mFoodList;
+    private Customer mCustomer;
 
+    public Order(int num){
+        this.mOrderNumber = num;
+    }
 
     public void setOrderNumber(int num){
-        mOrderNumber = num;
+        this.mOrderNumber = num;
     }
 
     public int getOrderNumber(){
@@ -26,5 +30,13 @@ public class Order {
 
     public List<Food> getFoodList(){
         return mFoodList;
+    }
+
+    public void setCustomer(Customer customer){
+        this.mCustomer = customer;
+    }
+
+    public Customer getCustomer(){
+        return mCustomer;
     }
 }
